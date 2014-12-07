df462ea975406b2a09f88fe4fe9f0b4a70b6d81f
Lehel Iszlai <iszlai.lehel@gmail.com>
2014-12-07 05:16:46 -0800

    Update

--- runMe.ps1
+++ runMe.ps1
@@ -1,11 +1,10 @@
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-
 echo "Installing apps"
 Read-Host 'Press Enter to continue...' | Out-Null
 choco install avgantivirusfree
 choco install google-chrome-x64 
 choco install vlc 
 choco install 7zip
+choco install delunge
 
 echo "Installing devtools"
 Read-Host 'Press Enter to continue...' | Out-Null
@@ -13,17 +12,22 @@ choco install sublimetext3
 choco install git 
 choco install githubforwindows 
 choco install putty
+choco install gow
+choco install listary
 choco install lockhunter
 
 echo "Installing Java tools"
 Read-Host 'Press Enter to continue...' | Out-Null
 choco install jdk8 
-choco install scala
+choco install eclipse
+choco install scala.install
 choco install sbt
 choco install gradle
+choco install maven
+
 
 echo "Installing Ruby"
 Read-Host 'Press Enter to continue...' | Out-Null
 choco install ruby
-choco install ruby.devkit
+choco install ruby2.devkit
  
